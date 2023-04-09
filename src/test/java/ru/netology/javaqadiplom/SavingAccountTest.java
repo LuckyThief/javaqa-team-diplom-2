901e1c02e11f2b79063245df6cc9a094d7467fdb
Komarov <komaroff74@yandex.ru>
2023-04-09 22:10:36 +0500

    change saving

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class SavingAccountTest {
 
-    @Test
+    @Test // Пополнение баланса
     public void shouldAddLessThanMaxBalance() {
         SavingAccount account = new SavingAccount(
                 2_000,
@@ -14,8 +14,51 @@ public class SavingAccountTest {
                 5
         );
 
-        account.add(3_000);
+        account.add(5_000);
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
+
+    @Test // Уменьшение баланса на сумму покупки
+    public void shouldPayReduceBalance() {
+        SavingAccount account = new SavingAccount(
+                3_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.pay(1_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+
+    }
+
+    @Test //Расчет процентов на остаток суммы
+    public void shouldYearChangeBalance() {
+        SavingAccount account = new SavingAccount(
+                1_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.yearChange();
+
+        Assertions.assertEquals(50, account.yearChange());
+    }
+
+    @Test //Должно появиться сообщение об ошибке, накопительная ставка всегда положительная
+    public void notSavingAccount() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    1_000,
+                    1_000,
+                    10_000,
+                    -5
+            );
+
+        });
+
+
+    }
+
 }
